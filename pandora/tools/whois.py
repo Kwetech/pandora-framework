46893c8f6eae1315aa3f3259e1658144e3cd9711
Mohammed Seidu <oxfordsec04@gmail.com>
2018-11-13 12:47:12 +0000

    updates

--- pandora/tools/whois.py
+++ pandora/tools/whois.py
@@ -5,18 +5,18 @@ def whois():
     using = "\33[91;1musing\33[00m(\33[92;1mwhois\33[00m) "
     host = input(using + "host<( ")
     ip = socket.gethostbyname(host)
-    sockt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sockt.connect_ex(("whois.arin.net", 80))
-    sockt.send(ip + "\r\n")
-
-    response = ""
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("whois.arin.net", 43))
+    
+    s.send((ip + "\r\n").encode())
+    
+    response = b""
     while True:
-        data = sockt.recv(4096)
+        data = s.recv(4096)
         response += data
         if not data:
             break
-    sockt.close()
+    s.close()
 
-    print('\t   +------------Details---------------+')
-    print(response)
-
+    print("\t+---------------Details---------------+")
+    print(response.decode())
